d718e9b1abdadaa584a5b710683d1ebabf628ec5
jiaola <djiao@jhu.edu>
2020-06-01 13:59:35 -0400

    Installed wagtailmenu

--- daydayupus/settings/base.py
+++ daydayupus/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'wagtail.search',
     'wagtail.admin',
     'wagtail.core',
+    'wagtailmenus',
 
     'modelcluster',
     'taggit',
@@ -78,6 +79,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
