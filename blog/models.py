68cb315016df4dcd336a1d87b4282137e864fb1d
jiaola <djiao@jhu.edu>
2020-06-01 08:10:28 -0400

    Refactor blog home and blog post page

--- blog/models.py
+++ blog/models.py
@@ -2,19 +2,33 @@ from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
+from wagtail.search import index
 
 
-class BlogPage(Page):
+class BlogIndexPage(Page):
     description = models.CharField(max_length=255, blank=True,)
 
     content_panels = Page.content_panels + [
         FieldPanel('description', classname='full')
     ]
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
-class PostPage(Page):
+
+class BlogPostPage(Page):
+    date = models.DateField('Post date')
     body = RichTextField(blank=True)
 
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
     content_panels = Page.content_panels + [
+        FieldPanel('date'),
         FieldPanel('body', classname='full')
     ]
