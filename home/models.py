69b907a7a7a034a559af7fb313a8c0d28bb8ea9b
jiaola <djiao@jhu.edu>
2020-06-29 08:21:51 -0400

    Update home page

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from puzzle.models import PuzzlePostPage, PuzzleIndexPage
 
 
 class HomePage(Page):
-    pass
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        puzzles = PuzzlePostPage.objects.live().order_by('-first_published_at')[:2]
+        context['today_puzzle'] = puzzles[0]
+        context['yesterday_puzzle'] = puzzles[1]
+        return context
