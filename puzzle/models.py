f795eae6bd969d8ee992f2bfb3d82644aea27572
jiaola <djiao@jhu.edu>
2020-06-28 20:26:37 -0400

    Add puzzles

---
+++ puzzle/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page, Orderable
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+
+
+class PuzzleIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request)
+        puzzles = self.get_children().live().order_by('-first_published_at')
+        context['puzzles'] = puzzles
+        return context
+
+
+class PuzzlePostPage(Page):
+    date = models.DateField('Post date')
+    body = RichTextField(blank=True)
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('body', classname='full'),
+        InlinePanel('gallery_images', label='Gallery images'),
+    ]
+
+
+class PuzzlePageGalleryImage(Orderable):
+    page = ParentalKey(PuzzlePostPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
