45251d9e249985d1955a317317ab24aa15770c9a
jiaola <djiao@jhu.edu>
2020-06-30 00:05:29 -0400

    Toggle button

--- puzzle/static/js/puzzle.js
+++ puzzle/static/js/puzzle.js
@@ -1,21 +1,28 @@
 // If URL contains hash, check if it's answer
 let answer = document.getElementById('puzzle-answer');
+let toggle = document.getElementById('puzzle-answer-toggle-button')
 let answer_style = 'none';
+let toggle_text = 'Show';
 if (window.location.hash) {
     let hash = window.location.hash.substring(1);
-    if (hash === 'answer') {
+    if (hash.toLowerCase() === 'answer') {
         document.title = document.title + ' - Answer'
         answer_style = 'block';
+        toggle_text = 'Hide'
     }
 }
 answer.style.display = answer_style
+toggle.innerHTML = toggle_text
 
 function toggleAnswer() {
     let answer = document.getElementById('puzzle-answer');
+    let toggle = document.getElementById('puzzle-answer-toggle-button')
     if (answer.style.display === 'none') {
         answer.style.display = 'block';
+        toggle.innerHTML = 'Hide';
     } else {
         answer.style.display = 'none';
+        toggle.innerHTML = 'Show';
     }
 }
 
