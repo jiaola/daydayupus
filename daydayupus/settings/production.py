3176166abb275b3172c4db09b2e3cc0eea123dce
jiaola <djiao@jhu.edu>
2020-06-30 15:33:44 -0400

    set SECURE_SSL_REDIRECT

--- daydayupus/settings/production.py
+++ daydayupus/settings/production.py
@@ -21,6 +21,7 @@ COMPRESS_CSS_HASHING_METHOD = 'content'
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
 
+SECURE_SSL_REDIRECT = True
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
